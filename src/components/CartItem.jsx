d060006659666a429caab27a9ecef12e762cc1be
Saugat <pdl.saugat07@gmail.com>
2025-09-03 09:34:02 +0545

    Cartitem update

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router";
 import { IoMdAdd, IoMdClose, IoMdRemove } from "react-icons/io";
+import { CartContext } from "../contexts/CartContext";
 
 const CartItem = ({ item }) => {
+  const { removeFromCart } = useContext(CartContext);
   const { id, title, image, price, amount } = item;
   return (
     <div className="flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500">
@@ -22,7 +24,10 @@ const CartItem = ({ item }) => {
               {title}
             </Link>
             {/* remove icon */}
-            <div className="text-xl cursor-pointer">
+            <div
+              onClick={() => removeFromCart(id)}
+              className="text-xl cursor-pointer"
+            >
               <IoMdClose className="text-gray-500 hover:text-red-500 transition" />
             </div>
           </div>
